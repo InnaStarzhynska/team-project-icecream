8f8e1cd189de9ef8892e32f0a532e754070faf04
InnaStarzhynska <113523884+InnaStarzhynska@users.noreply.github.com>
2022-12-20 13:51:06 +0000

    Deploying to gh-pages from @ InnaStarzhynska/team-project-icecream@e96174bd44a63c8e2f69b65e0f1c935e07ee7092 🚀

--- index.38efbe95.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{slidesPerView:4,loop:!0,spaceBetween:31,freeMode:!0,autoplay:{delay:1e3}});
-//# sourceMappingURL=index.38efbe95.js.map
---
+++ index.bf542034.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!1,adapptiveHeight:!0,slidesToShow:1,slidesToScroll:1,speed:2e3,fade:!0,easing:"linear",infinite:!0,autoplay:!0,autoplaySpeed:1e3,pauseOnFocus:!0,pauseOnHover:!0})}));
+//# sourceMappingURL=index.bf542034.js.map
